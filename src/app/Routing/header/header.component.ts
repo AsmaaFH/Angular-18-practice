6f0a37b2437091fcf92d9f98f38abf06f9b4dbdc
Asmaa <asmaafhk@gmail.com>
2024-11-23 16:23:27 +0200

    login

--- src/app/Routing/header/header.component.ts
+++ src/app/Routing/header/header.component.ts
@@ -1,16 +1,23 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   private router = inject(Router);
   private route = inject(ActivatedRoute);
+  public auth = inject(AuthService);
+
+  ngOnInit(){
+    console.log(this.auth.isLoggedIn());
+  }
   goSearch(){
     let part = "foo";
     this.router.navigate(['search', part, 'moo']).then(success => {
@@ -18,5 +25,11 @@ export class HeaderComponent {
     }).catch(err => {
       console.error('Navigation error:', err);
     });
+  }
+  logout(){
+    this.auth.logout();
+  }
+  login(){
+    this.auth.login();
   }
 }
